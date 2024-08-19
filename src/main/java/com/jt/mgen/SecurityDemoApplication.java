ce105601e4558a16d1db990a8585d73fd5eee527
Rohan Kumar Tammineedi <RTMT1067@MGTECHSOFT.IN>
2024-08-19 15:02:02 +0530

    app properties login

--- src/main/java/com/jt/mgen/SecurityDemoApplication.java
+++ src/main/java/com/jt/mgen/SecurityDemoApplication.java
@@ -2,12 +2,14 @@ package com.jt.mgen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+//        (exclude = SecurityAutoConfiguration.class)
 public class SecurityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoApplication.class, args);
+    }
 
 }
