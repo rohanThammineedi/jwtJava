6d236c9acdaab791cccf03505c7bf424ba846905
Rohan Kumar Tammineedi <RTMT1067@MGTECHSOFT.IN>
2024-08-19 14:32:03 +0530

    jwt implementation

---
+++ src/test/java/com/jt/mgen/SecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt.mgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
